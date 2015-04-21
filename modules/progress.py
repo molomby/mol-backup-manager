cf3091503539b9e70d5fe1384471ef9a749155a7
John Molomby <john@molomby.com>
2015-04-21 15:36:13 +1000

    Tweaks for upload progress on Linux and example config

--- modules/progress.py
+++ modules/progress.py
@@ -2,17 +2,17 @@
 from __future__ import print_function
 import os
 from datetime import datetime
+import sys
 
 
 class S3Progress(object):
 	def __init__(self, local_path, indent=6, bar_width=50):
 		self.started = False
 		self.filesize = os.path.getsize(local_path)
-		self.num_cb = int(self.filesize / (1024 * 1024)) # Once for each x Kb
+		self.num_cb = int(self.filesize / (1024 * 1024 * 1)) # Once for each X Mb
 		self.indent = (" " * indent)
 		self.bar_width = bar_width
-
-
+		
 	def start(self):
 		print(self.indent + "Starting...", end='')
 
@@ -29,6 +29,7 @@ class S3Progress(object):
 		self.avgKbs = "{:,.2f}".format((complete / 1024) / elapsed) if elapsed > 0 else "--.--"
 		
 		print(self.indent + "[" + ("="*comp) + ">" + ("-"*incomp) + "] " + str(perc)  +  "% at " + self.avgKbs + " KB/s", end="\r")
+		sys.stdout.flush()
 
 	def final(self):
 		print(self.indent + "[" + ("="*(self.bar_width-2)) + "] 100% at " + self.avgKbs + " KB/s     ") # trailing space intentional
